23357b77fe4532d5712e6e22fe81142f78e6e68e
Andersonpoanorte <andersonpoanorte1@gmail.com>
2024-08-22 10:55:41 -0300

    commit inicial

---
+++ src/exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+//função de multiplicação
+function multiplicar(numero1, numero2) {
+    return numero1 * numero2;
+}
+console.log(multiplicar(2, 4));
+// função de saudação
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+console.log(dizOla('Anderson'));
---
+++ src/exercicio.ts
@@ -0,0 +1,14 @@
+//função de multiplicação
+function multiplicar(numero1: number, numero2: number ): number {
+    return numero1 * numero2;
+}
+
+console.log(multiplicar(2, 4));
+
+// função de saudação
+
+function dizOla(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(dizOla('Anderson'));
